4a03c2455a08f4cbcd198ecf4ae1b2a213bea429
E-Hizzle <eho.rutgers@gmail.com>
2016-03-16 17:22:50 -0400

    Changed color of submit button.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,5 +4,5 @@ $("#goButton").click(function() {
 
   //Clicking the button hides the input boxes and buttons, it'll show the event divs that will be dynamically created based on input.
   $("#inputSection").hide();
-
+  // $("#eventDiv").show();
 });
