cc13f037447df839b72f023f3284a38c3f765692
E-Hizzle <eho.rutgers@gmail.com>
2016-03-16 15:51:17 -0400

    First push.

---
+++ assets/javascript/logic.js
@@ -0,0 +1,8 @@
+//Click function for Let's Go Button.
+$("#goButton").click(function() {
+  console.log(this);
+
+  //Clicking the button hides the input boxes and buttons, it'll show the event divs that will be dynamically created based on input.
+  $("#inputSection").hide();
+
+});
